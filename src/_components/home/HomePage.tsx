55199a339e8d76d1a96fc7e015ac5a585a5b0538
mrNcob <mariandobre002@gmail.com>
2025-10-02 15:33:41 +0300

    Finishing the home page and update the css variables system to accept dynamic opacity values

--- src/_components/home/HomePage.tsx
+++ src/_components/home/HomePage.tsx
@@ -1,3 +1,22 @@
+import './HomePage.css';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className='home-page-container'>
+      <div className='home-page-hero-background-container'>
+        <img
+          src='./home-hero-background-edited.jpg'
+          alt=''
+          loading='lazy'
+          className='home-page-hero-background'
+        />
+      </div>
+
+      <div className='home-page-content-container'>
+        <h1 className='home-page-content-heading'>
+          Your&nbsp;<span>One-Stop Shop</span>&nbsp;for Everything You Need
+        </h1>
+      </div>
+    </div>
+  );
 }
